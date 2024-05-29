61a236ce00c2b28402467a0d6fa42ce56e353ff5
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2024-05-29 09:16:57 +0200

    add tests for closure

--- chapter4/closure.py
+++ chapter4/closure.py
@@ -1,3 +1,6 @@
+# modify the multiplier below so that it returns 
+# the expected result:
+# [2, 4, 6, 8, 10]
 def make_multiplier_of(n):
     mylist = []
     for i in n:
@@ -6,7 +9,14 @@ def make_multiplier_of(n):
         mylist.append(multiplier)
     return mylist
 
-times_x = make_multiplier_of(list(range(5)))
+# for testing purposes, we wrap this in a function
+def main():
+    times_x = make_multiplier_of(list(range(5)))
+    times_table = []
+    for times in times_x:
+        print(times(2))
+        times_table.append(times(2))
+    return times_table
 
-for times in times_x:
-    print(times(2))
+if __name__ == "__main__":
+    main()
